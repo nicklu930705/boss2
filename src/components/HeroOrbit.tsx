1ddad621f2e2a2ad1485ad4bf205ff54c953ccc1
Lu TSEHSEIN <NickLu930705@gmail.com>
2026-09-21 03:03:47 +0800

    fix: update 45L bag image path to 規格圖__IMG_0222.JPG

--- src/components/HeroOrbit.tsx
+++ src/components/HeroOrbit.tsx
@@ -134,7 +134,7 @@ export default function HeroOrbit() {
     
     // Override image for specific cleaning bag
     if (id === '01_清潔袋-01_一般捲取式-大_45L') {
-      coverImg = '/assets/01_清潔袋/01_一般捲取式/大_45L/taisu_bag_large_45L.jpeg';
+      coverImg = '/assets/01_清潔袋/01_一般捲取式/大_45L/規格圖__IMG_0222.JPG';
     }
 
     return {
